9b5a62e6887bf6d850d644e753fd21ac7e454d4d
Daniel Viegas <viegasdll@gmail.com>
2023-01-22 19:45:44 -0800

    Linear regression: add predict function

--- dvml/models/linear.py
+++ dvml/models/linear.py
@@ -1,6 +1,8 @@
 """
 Module for linear models
 """
+import numpy as np
+
 from dvml.models.model import SupervisedModel
 
 
@@ -13,11 +15,31 @@ class LinearRegression(SupervisedModel):
         self.params = None
 
     def predict(self, x_pred):
-        pass
+        """
+        Prediction function
+        :param x_pred:
+        :return:
+        """
+
+        # First, check that the model is trained, or at least has parameters
+        if self.params is None:
+            raise RuntimeError("Model must be trained before predicting.")
+
+        # Convert the input to a NumPy array, and format for linear regression
+        x_np = np.array(x_pred)
+        x_in = np.concatenate(
+            (np.ones(len(x_np)).reshape([len(x_np), 1]), x_np), axis=1
+        )
+
+        return np.dot(x_in, self.params)
 
     def train(self, x_train, y_train):
         pass
 
     def set_params(self, params):
-        """Set model parameters to input `params`"""
-        self.params = params
+        """
+        Sets the model parameters
+        :param params:
+        :return:
+        """
+        self.params = np.array(params).reshape(len(params), 1)
--- tests/models/test_linear.py
+++ tests/models/test_linear.py
@@ -1,13 +1,35 @@
+import unittest
+
 import numpy as np
 
 from dvml.models.linear import LinearRegression
-import pytest
 
 
-def test_set_params():
-    model = LinearRegression()
-    test_params = [0.1, 0.2, 0.3]
+class TestLinearRegression(unittest.TestCase):
+    def test_set_params(self):
+        model = LinearRegression()
+        test_params = [0.1, 0.2, 0.3]
+        formatted_params = np.array(test_params).reshape(3, 1)
 
-    model.set_params(test_params)
+        model.set_params(test_params)
 
-    assert np.array_equal(model.params, test_params)
+        self.assertTrue(np.array_equal(model.params, formatted_params))
+
+    def test_predict_error(self):
+        model = LinearRegression()
+
+        self.assertRaises(RuntimeError, model.predict, None)
+
+    def _test_predict(self, y, x_pred, params):
+        model = LinearRegression()
+        model.set_params(params)
+
+        y_form = np.array(y).reshape([len(y), 1])
+
+        self.assertTrue(np.array_equal(model.predict(x_pred), y_form))
+
+    def test_predict_01(self):
+        self._test_predict([20], np.array([1, 2, 3]).reshape([1, 3]), [6, 1, 2, 3])
+
+    def test_predict_02(self):
+        self._test_predict([9, 3, 7], np.array([[0, 2], [-1, 1], [3, 0]]), [1, 2, 4])
