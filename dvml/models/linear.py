c7d6919c1de65c4913d1787eb7d94343f13fbfe3
Daniel Viegas <viegasdll@gmail.com>
2023-01-22 18:20:39 -0800

    Add linear regression class skeleton

---
+++ dvml/models/linear.py
@@ -0,0 +1,16 @@
+"""
+Module for linear models
+"""
+from dvml.models.model import SupervisedModel
+
+
+class LinearRegression(SupervisedModel):
+    """
+    Placeholder for doc
+    """
+
+    def predict(self, x_pred):
+        pass
+
+    def train(self, x_train, y_train):
+        pass
