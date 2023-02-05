0f26689f9f92cf7b05bc23c2a465b3e2e04d80c4
Daniel Viegas <viegasdll@gmail.com>
2023-02-04 18:38:09 -0800

    Classification tree: add tree node prediction function

--- dvml/models/classification_tree.py
+++ dvml/models/classification_tree.py
@@ -1,6 +1,8 @@
 """
 Module for classification tree
 """
+import numpy as np
+
 from dvml.models.model import SupervisedModel
 
 
@@ -9,16 +11,42 @@ class ClassificationTreeNode(SupervisedModel):
     Individual node for a classification tree
     """
 
-    def __init__(self):
+    def __init__(self, return_val=0.5):
         # Define the left and right successors, set to empty to start with
         self.left = None
         self.right = None
         # Set a default value, in case it becomes a leaf node
-        # The training process will either define successors, or update the decision
-        self.decision = 0.5
+        # The training process will either define successors, or update the return value
+        self.return_val = return_val
+        # Initialize a dummy decision function
+        self.decision = {
+            "feature": 0,  # The index of the feature used in the decision
+            "boundary": 0,  # The boundary value that decides between left/right successor
+        }
 
     def predict(self, x_in):
-        pass
+        # Convert x_in to a numpy array
+        x_form = np.array(x_in)
+        # Make it into a matrix if it was just a vector
+        if x_form.ndim == 1:
+            x_form = x_form.reshape([1, len(x_form)])
+
+        # Compute the prediction for each row of the dataset, and collect them into a vector
+        return np.apply_along_axis(self.predict_one, axis=1, arr=x_form)
+
+    def predict_one(self, x_vec):
+        """
+
+        :param x_vec:
+        :return:
+        """
+        # If either successor is not defined, return the existing return value
+        if self.left is None or self.right is None:
+            return self.return_val
+        # Otherwise, apply the decision function and collect the return value from the successor
+        if x_vec[self.decision["feature"]] < self.decision["boundary"]:
+            return self.left.predict_one(x_vec)
+        return self.right.predict_one(x_vec)
 
     def train(self, x_train, y_train, conf: dict = None):
         pass
--- tests/models/test_classification_tree.py
+++ tests/models/test_classification_tree.py
@@ -1,6 +1,9 @@
 import unittest
 
+import numpy as np
+
 from dvml.models.classification_tree import ClassificationTreeNode
+from numpy.testing import assert_array_equal
 
 
 class TestClassificationTreeNode(unittest.TestCase):
@@ -9,4 +12,85 @@ class TestClassificationTreeNode(unittest.TestCase):
 
         self.assertTrue(node.left is None)
         self.assertTrue(node.right is None)
-        self.assertEqual(node.decision, 0.5)
+        self.assertEqual(node.return_val, 0.5)
+
+    def test_predict_one_01(self):
+        node = ClassificationTreeNode(return_val=0.2)
+        x_vec = np.array([1, 2, 3])
+
+        expected = 0.2
+
+        self.assertEqual(node.predict_one(x_vec), expected)
+
+    def test_predict_one_02(self):
+        node = ClassificationTreeNode()
+
+        left = ClassificationTreeNode(0.2)
+        right = ClassificationTreeNode(0.8)
+
+        node.left = left
+        node.right = right
+
+        x_vec = np.array([1, 2, 3])
+
+        node.decision = {
+            "feature": 1,
+            "boundary": 4,
+        }
+
+        expected = 0.2
+
+        self.assertEqual(node.predict_one(x_vec), expected)
+
+    def test_predict_one_03(self):
+        node = ClassificationTreeNode()
+
+        left = ClassificationTreeNode(0.2)
+        right = ClassificationTreeNode(0.8)
+
+        node.left = left
+        node.right = right
+
+        x_vec = np.array([1, 2, 3])
+
+        node.decision = {
+            "feature": 2,
+            "boundary": 1,
+        }
+
+        expected = 0.8
+
+        self.assertEqual(node.predict_one(x_vec), expected)
+
+    def test_predict_single_vec(self):
+        node = ClassificationTreeNode(return_val=0.2)
+        x_vec = np.array([1, 2, 3])
+
+        expected = [0.2]
+
+        assert_array_equal(node.predict(x_vec), expected)
+
+    def test_predict_matrix(self):
+        node = ClassificationTreeNode()
+
+        left = ClassificationTreeNode(0.2)
+        right = ClassificationTreeNode(0.8)
+
+        node.left = left
+        node.right = right
+
+        node.decision = {
+            "feature": 2,
+            "boundary": 1,
+        }
+
+        x_in = [
+            [1, 2, 3],
+            [0, -1, -1],
+            [10, 15, 1],
+        ]
+
+        result = node.predict(x_in)
+        expected = [0.8, 0.2, 0.8]
+
+        assert_array_equal(result, expected)
